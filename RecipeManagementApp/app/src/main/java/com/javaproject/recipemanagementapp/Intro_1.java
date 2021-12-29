f63a14465168114ea53697aa6b2775a2e853cd32
Mayank Patel <mayank509.mp@gmail.com>
2021-12-30 00:29:39 +0530

    added intro page 1 with temp logo and text
    a rough work of intro page 1

---
+++ RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Intro_1.java
@@ -0,0 +1,14 @@
+package com.javaproject.recipemanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intro_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro1);
+    }
+}
