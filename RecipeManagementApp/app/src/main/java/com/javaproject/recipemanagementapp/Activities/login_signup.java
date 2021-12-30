6496cf09a4d6e15bdaa44819de449037edd18426
Mayank Patel <mayank509.mp@gmail.com>
2021-12-30 17:20:34 +0530

    Pages upto Login added

---
+++ RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/login_signup.java
@@ -0,0 +1,16 @@
+package com.javaproject.recipemanagementapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.javaproject.recipemanagementapp.R;
+
+public class login_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+    }
+}
