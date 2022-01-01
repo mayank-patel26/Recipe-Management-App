894d0bf09d07bc5ea455361919742dd3178f8cb9
Mayank Patel <mayank509.mp@gmail.com>
2022-01-01 20:10:08 +0530

    added all recipe page
    colors can be adjusted later

---
+++ RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/all_recipe.java
@@ -0,0 +1,18 @@
+
+package com.javaproject.recipemanagementapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+
+import com.javaproject.recipemanagementapp.R;
+
+public class all_recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_recipe);
+    }
+}
