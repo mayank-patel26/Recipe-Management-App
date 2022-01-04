348f5e82ca6b888b5bd9c493733514da3da4c1e5
Mayank Patel <mayank509.mp@gmail.com>
2022-01-04 13:49:41 +0530

    added navigation code

--- RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/login_signup.java
+++ RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/login_signup.java
@@ -2,7 +2,9 @@ package com.javaproject.recipemanagementapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.javaproject.recipemanagementapp.R;
 
@@ -12,5 +14,17 @@ public class login_signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
+
+        Button l_s_login_btn = findViewById(R.id.l_s_login_btn);
+        l_s_login_btn.setOnClickListener(view -> {
+            Intent i1 = new Intent(login_signup.this, login.class);
+            startActivity(i1);
+        });
+
+        Button l_s_signup_btn = findViewById(R.id.l_s_signup_btn);
+        l_s_signup_btn.setOnClickListener(view -> {
+            Intent i2 = new Intent(login_signup.this, signup.class);
+            startActivity(i2);
+        });
     }
 }
