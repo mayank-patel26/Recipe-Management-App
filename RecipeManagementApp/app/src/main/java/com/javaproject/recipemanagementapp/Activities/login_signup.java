5e05ca8f417ca9761a253d0c22ac0c612c5adea6
Mayank Patel <mayank509.mp@gmail.com>
2022-01-04 12:51:50 +0530

    updated intro page

--- RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/login_signup.java
+++ RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/login_signup.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.javaproject.recipemanagementapp.R;
 
-public class Intro_2 extends AppCompatActivity {
+public class login_signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro2);
+        setContentView(R.layout.activity_login_signup);
     }
 }
