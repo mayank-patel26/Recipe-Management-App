55c1cdaaadf950f27db8f16b84a0dee90f071bb4
Mayank Patel <mayank509.mp@gmail.com>
2022-01-23 20:15:25 +0530

    Remember Me feature added
    all the recipes are retrieved instead of user specific ones

--- RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/StartupPage.java
+++ RecipeManagementApp/app/src/main/java/com/javaproject/recipemanagementapp/Activities/StartupPage.java
@@ -16,11 +16,33 @@ public class StartupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+
         DatabaseHelper.setDB(this);
-        Button get_started = findViewById(R.id.intro_skip);
-        get_started.setOnClickListener(view -> {
-            Intent intent = new Intent(StartupPage.this, intro_scroll.class);
-            startActivity(intent);
-        });
+
+        if(DatabaseHelper.checkRemStatus()){
+            String setEmail = DatabaseHelper.getRemEmail();
+            DatabaseHelper.checkemail(setEmail);
+//            DatabaseHelper.getAllRecipe();
+            Button get_started = findViewById(R.id.intro_skip);
+            get_started.setOnClickListener(view -> {
+                Intent toLandingPage = new Intent(StartupPage.this, landing_page.class);
+                startActivity(toLandingPage);
+            });
+        }
+        else if(!DatabaseHelper.checkRemStatus()){
+            Button get_started = findViewById(R.id.intro_skip);
+            get_started.setOnClickListener(view -> {
+                Intent intent = new Intent(StartupPage.this, intro_scroll.class);
+                startActivity(intent);
+            });
+        }
+
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent toExit = new Intent(this, ExitPage.class);
+        startActivity(toExit);
     }
 }
